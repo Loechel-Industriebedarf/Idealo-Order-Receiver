2d9c91bcd9401f7ef5d2d9dc0e3ba1f6a95f49e5
ADarkHero <maik.riedlsperger@gmail.com>
2021-10-07 09:35:37 +0200

    Corrected fees for our erp system

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 			
 			
 			
-			//$result = setMerchantOrderNumbers($orderNumbers, $url, $shopid, $accessToken);
+			$result = setMerchantOrderNumbers($orderNumbers, $url, $shopid, $accessToken);
 
 			
 			//Debug Stuff.
@@ -238,7 +238,7 @@
 				$csv = $csv . $orderContent["payment"]["paymentMethod"] . ";"; //PaymentMethod
 				$csv = $csv . $orderContent["payment"]["transactionId"] . ";"; //TransactionId
 				$csv = $csv . $orderContent["shippingCosts"] . ";"; //TransactionId
-				$csv = $csv . floatval($lineItem["price"])*0.09 . ";"; //9% Fees
+				$csv = $csv . floatval($lineItem["price"])/floatval($lineItem["quantity"])*0.09 . ";"; //9% Fees
 				$csv = $csv . "\r\n";
 			}
 			error_reporting(-1);
